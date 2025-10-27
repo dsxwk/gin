6d250ddb42c0613cccf70c101c18ceecd2b093d9
dsx <25076778@qq.com>
2025-10-27 11:44:14 +0800

    更新公共http请求包

--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.1
 	github.com/swaggo/swag v1.16.6
+	github.com/valyala/fasthttp v1.68.0
 	go.uber.org/zap v1.27.0
 	golang.org/x/crypto v0.43.0
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
@@ -30,6 +31,7 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/andybalholm/brotli v1.2.0 // indirect
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic v1.14.1 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
@@ -82,6 +84,7 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/otel v1.38.0 // indirect
 	go.opentelemetry.io/otel/metric v1.38.0 // indirect
--- utils/http.go
+++ utils/http.go
@@ -1,79 +1,112 @@
 package utils
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
+	"github.com/valyala/fasthttp"
+	"net/url"
+	"strings"
 	"time"
 )
 
-var HttpClient = &http.Client{
-	Timeout: 5 * time.Second,
-	Transport: &http.Transport{
-		MaxIdleConns:        100,              // 设置最大空闲连接数
-		MaxIdleConnsPerHost: 100,              // 设置每个主机的最大空闲连接数
-		IdleConnTimeout:     90 * time.Second, // 设置空闲连接超时时间
-	},
+const timeout = 5 * time.Second
+
+type HttpOption struct {
+	Headers map[string]string // 请求头
+	Query   map[string]string // get参数
+	Form    map[string]string // 表单参数
+	Body    []byte            // body参数
+	Timeout time.Duration     // 超时时间
+}
+
+// buildURL 拼接get请求query参数
+func buildUrl(baseURL string, query map[string]string) string {
+	if len(query) == 0 {
+		return baseURL
+	}
+	q := url.Values{}
+	for k, v := range query {
+		q.Set(k, v)
+	}
+	if strings.Contains(baseURL, "?") {
+		return baseURL + "&" + q.Encode()
+	}
+	return baseURL + "?" + q.Encode()
 }
 
 // HttpRequest 发送http请求
-func HttpRequest(method, url string, body []byte, headers map[string]string) (string, error) {
-	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
-	if err != nil {
-		return "", fmt.Errorf("创建请求失败: %w", err)
+func HttpRequest(method, uri string, opt *HttpOption) (string, error) {
+	req := fasthttp.AcquireRequest()
+	resp := fasthttp.AcquireResponse()
+	defer func() {
+		fasthttp.ReleaseRequest(req)
+		fasthttp.ReleaseResponse(resp)
+	}()
+
+	if opt == nil {
+		opt = &HttpOption{}
+	}
+	if opt.Timeout == 0 {
+		opt.Timeout = timeout
 	}
 
-	// 设置默认header
-	if headers == nil {
-		headers = map[string]string{
-			"Content-Type": "application/json",
+	method = strings.ToUpper(method)
+	uri = buildUrl(uri, opt.Query)
+	req.Header.SetMethod(method)
+	req.SetRequestURI(uri)
+	contentType := "application/json"
+	if len(opt.Form) > 0 {
+		// x-www-form-urlencoded
+		values := url.Values{}
+		for k, v := range opt.Form {
+			values.Set(k, v)
 		}
-	}
-	if !HasKey(headers, "Content-Type") {
-		headers["Content-Type"] = "application/json"
+		req.SetBodyString(values.Encode())
+		contentType = "application/x-www-form-urlencoded"
+	} else if len(opt.Body) > 0 {
+		// body
+		req.SetBody(opt.Body)
+		contentType = "application/json"
 	}
 
-	// 设置header
-	for k, v := range headers {
+	// header设置
+	req.Header.Set("Content-Type", contentType)
+	for k, v := range opt.Headers {
 		req.Header.Set(k, v)
 	}
 
-	// 使用全局客户端
-	client := HttpClient
-	resp, err := client.Do(req)
-	if err != nil {
+	// 创建客户端并设置超时
+	client := &fasthttp.Client{
+		MaxConnsPerHost: 100,     // 最大连接数
+		ReadTimeout:     timeout, // 读取超时
+		WriteTimeout:    timeout, // 写入超时
+	}
+
+	// 发送请求
+	if err := client.Do(req, resp); err != nil {
 		return "", fmt.Errorf("请求失败: %w", err)
 	}
-	defer func(Body io.ReadCloser) {
-		err = Body.Close()
-		if err != nil {
-			fmt.Println("关闭响应失败: ", err)
-		}
-	}(resp.Body)
 
-	// 读取响应体
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", fmt.Errorf("读取响应失败: %w", err)
+	// 获取响应内容
+	respBody := string(resp.Body())
+
+	if resp.StatusCode() != 200 {
+		return "", fmt.Errorf("请求失败, 状态码: %d, 响应: %s", resp.Header.StatusCode(), respBody)
 	}
 
-	return string(respBody), nil
+	return respBody, nil
 }
 
-// HttpRequestJson 发送json请求并解析响应为结构体
-func HttpRequestJson[T any](method, url string, body []byte, headers map[string]string) (*T, error) {
-	// 执行请求
-	resp, err := HttpRequest(method, url, body, headers)
+// HttpRequestJson 发送请求并解析json响应
+func HttpRequestJson[T any](method, url string, opt *HttpOption) (*T, error) {
+	resp, err := HttpRequest(method, url, opt)
 	if err != nil {
 		return nil, err
 	}
 
-	// 解析响应
 	var result T
 	if err = json.Unmarshal([]byte(resp), &result); err != nil {
-		return nil, fmt.Errorf("json解析失败: %w", err)
+		return nil, fmt.Errorf("json解析失败: %w\n响应内容:\n%s", err, resp)
 	}
 
 	return &result, nil
